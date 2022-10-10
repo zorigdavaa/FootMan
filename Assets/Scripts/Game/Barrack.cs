5ed8accf09d9c09dc8567956347a8ab80170f0cf
zorigdavaa <zorigoo.nexus@yahoo.com>
2022-10-10 15:02:27 +0800

    ss

---
+++ Assets/Scripts/Game/Barrack.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrack : Zone
+{
+    public override void SwallowMaterial(Transform item, int price)
+    {
+        StartCoroutine(localFunction(item));
+        IEnumerator localFunction(Transform item)
+        {
+            float time = 0;
+            float duration = SwallowDuration;
+            Vector3 itemStartPos = item.position;
+            while (time < duration)
+            {
+                time += Time.deltaTime;
+                item.position = Vector3.Lerp(itemStartPos, transform.position, time / duration);
+                yield return null;
+            }
+            if (price == 0)
+            {
+                Instantiate(InstantiateObj, insPos.position, Quaternion.identity);
+                Price = 1;
+            }
+            Destroy(item.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Game/Zone.cs
+++ Assets/Scripts/Game/Zone.cs
@@ -7,7 +7,11 @@ public class Zone : MonoBehaviour
 {
     [SerializeField] List<GameObject> icons;
     [SerializeField] TextMeshPro priceText;
-    [SerializeField] int Price;
+    public Transform insPos;
+    public int Price;
+    public float SwallowDelay = 0.04f;
+    public float SwallowDuration = 0.5f;
+    public GameObject InstantiateObj;
 
 
     Inventory Inventory;
@@ -19,8 +23,13 @@ public class Zone : MonoBehaviour
     private void OnTriggerStay(Collider other)
     {
         WaitTime -= Time.deltaTime;
-        if (other.gameObject.CompareTag("Player") && WaitTime < 0 && Price > 0 && Inventory.HasItem())
+        if (other.gameObject.CompareTag("Player") && WaitTime < 0 && Price > 0)
         {
+            Inventory = other.GetComponent<Player>().inventory;
+            if (!Inventory.HasItem())
+            {
+                return;
+            }
             Transform item = Inventory.GetLastItem().transform;
             if (item)
             {
@@ -28,23 +37,28 @@ public class Zone : MonoBehaviour
                 // priceText.text = Price.ToString();
                 SwallowMaterial(item, Price);
             }
-            WaitTime = 0.04f;
+            WaitTime = SwallowDelay;
         }
     }
 
-    void SwallowMaterial(Transform item, int price)
+    public virtual void SwallowMaterial(Transform item, int price)
     {
         StartCoroutine(localFunction(item));
         IEnumerator localFunction(Transform item)
         {
             float time = 0;
-            float duration = 0.1f;
+            float duration = SwallowDuration;
             Vector3 itemStartPos = item.position;
             while (time < duration)
             {
                 time += Time.deltaTime;
                 item.position = Vector3.Lerp(itemStartPos, transform.position, time / duration);
                 yield return null;
+            }
+            if (price == 0)
+            {
+                Instantiate(InstantiateObj, insPos.position, Quaternion.identity);
+                Destroy(gameObject);
             }
             Destroy(item.gameObject);
         }
