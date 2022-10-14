ada9392f1599dccdfaaeaebf46e5728b784f680f
zorigdavaa <zorigoo.nexus@yahoo.com>
2022-10-14 12:46:54 +0800

    ss

--- Assets/Scripts/Game/Barrack.cs
+++ Assets/Scripts/Game/Barrack.cs
@@ -2,33 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Barrack : Zone
+public class Barrack : MonoBehaviour
 {
+    public GameObject InstantiateObj;
+    public Transform insPos;
     [SerializeField] List<Transform> jagsahPos;
     int jagsahIndex = 0;
+    public int capacity = 5;
+    float WaitTime = 0;
+    float DelayTime = 3;
     // [SerializeField] List<>
-    public override void SwallowItem(Transform item, int price)
+    private void Update()
     {
-        StartCoroutine(localFunction(item));
-        IEnumerator localFunction(Transform item)
+        if (capacity > jagsahIndex)
         {
-            float time = 0;
-            float duration = SwallowDuration;
-            Vector3 itemStartPos = item.position;
-            while (time < duration)
-            {
-                time += Time.deltaTime;
-                item.position = Vector3.Lerp(itemStartPos, transform.position, time / duration);
-                yield return null;
-            }
-            if (price == 0)
+            WaitTime -= Time.deltaTime;
+            if (WaitTime < 0)
             {
                 Bot insBot = Instantiate(InstantiateObj, insPos.position + new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-2f, -1f)), Quaternion.identity).GetComponent<Bot>();
                 insBot.GotoPos(jagsahPos[jagsahIndex].position);
                 jagsahIndex++;
-                Price = 1;
+                WaitTime = DelayTime;
             }
-            Destroy(item.gameObject);
         }
     }
+    // public override void SwallowItem(Transform item, int price)
+    // {
+    //     StartCoroutine(localFunction(item));
+    //     IEnumerator localFunction(Transform item)
+    //     {
+    //         float time = 0;
+    //         float duration = SwallowDuration;
+    //         Vector3 itemStartPos = item.position;
+    //         while (time < duration)
+    //         {
+    //             time += Time.deltaTime;
+    //             item.position = Vector3.Lerp(itemStartPos, transform.position, time / duration);
+    //             yield return null;
+    //         }
+    //         if (price == 0)
+    //         {
+    //             Bot insBot = Instantiate(InstantiateObj, insPos.position + new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-2f, -1f)), Quaternion.identity).GetComponent<Bot>();
+    //             insBot.GotoPos(jagsahPos[jagsahIndex].position);
+    //             jagsahIndex++;
+    //             Price = 1;
+    //         }
+    //         Destroy(item.gameObject);
+    //     }
+    // }
 }
