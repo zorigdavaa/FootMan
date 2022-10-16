e15651cb24c43edc1718b9e04fa666c284b80d76
Zorigdavaa <zorigoo.nexus@yahoo.com>
2022-10-16 18:22:41 +0800

    ss

--- Assets/Scripts/Game/MoneySucker.cs
+++ Assets/Scripts/Game/MoneySucker.cs
@@ -3,18 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using ZPackage;
 
 public class MoneySucker : MonoBehaviour
 {
     [SerializeField] List<GameObject> icons;
     [SerializeField] TextMeshPro priceText;
+    [SerializeField] Image fillImage;
     public Transform insPos;
-    public int Price;
+    public int Price, CollectedMoney;
     public GameObject monePrefab;
     float SwallowDelay = 1f;
     public GameObject InstantiateObj;
     float WaitTime = 0;
+    private void Start()
+    {
+        fillImage.fillAmount = 0;
+    }
     private void OnTriggerStay(Collider other)
     {
         WaitFindSwallow(other);
@@ -23,19 +29,19 @@ public class MoneySucker : MonoBehaviour
     public virtual void WaitFindSwallow(Collider other)
     {
         WaitTime -= Time.deltaTime;
-        if (other.gameObject.CompareTag("Player") && WaitTime < 0 && Price > 0 && GameManager.Instance.Coin > 0)
+        if (other.gameObject.CompareTag("Player") && WaitTime < 0 && CollectedMoney < Price && GameManager.Instance.Coin > 0)
         {
             GameManager.Instance.Coin--;
-            Price--;
-            InsMoneyAndSuck(other, Price);
-
+            CollectedMoney++;
+            fillImage.fillAmount = (float)CollectedMoney / (float)Price;
+            InsMoneyAndSuck(other, CollectedMoney);
             WaitTime = SwallowDelay;
         }
     }
 
-    private void InsMoneyAndSuck(Collider other, int price)
+    private void InsMoneyAndSuck(Collider other, int curColMoney)
     {
-        GameObject money = Instantiate(monePrefab, other.transform.position + Vector3.up * 3, Quaternion.identity);
+        GameObject money = Instantiate(monePrefab, other.transform.position + Vector3.up * 5, Quaternion.identity);
         StartCoroutine(localCoroutine());
         IEnumerator localCoroutine()
         {
@@ -50,7 +56,7 @@ public class MoneySucker : MonoBehaviour
                 // Mathf.Lerp(0, 1, t);
                 yield return null;
             }
-            if (price == 0)
+            if (curColMoney == Price)
             {
                 Instantiate(InstantiateObj, insPos.position, Quaternion.identity);
                 Destroy(gameObject);
--- Assets/Scripts/Game/StandInterActer.cs
+++ Assets/Scripts/Game/StandInterActer.cs
@@ -2,22 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
 
 public class StandInterActer : MonoBehaviour
 {
-    float WaitTime = 3;
+    [SerializeField] Image fillImage;
+    float WaitTime = 3, currentTime;
     public EventHandler OnStandedStill;
 
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            WaitTime -= Time.deltaTime;
-            if (WaitTime < 0)
+            currentTime += Time.deltaTime;
+            fillImage.fillAmount = currentTime / WaitTime;
+            if (currentTime > WaitTime)
             {
-                WaitTime = 3;
+                currentTime = 0;
                 OnStandedStill?.Invoke(this, EventArgs.Empty);
             }
         }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        fillImage.fillAmount = 0;
+        currentTime = 0;
     }
 }
